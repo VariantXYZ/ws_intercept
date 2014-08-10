c665e36752d10f68b0f0bba641be90809bcb2dae
Vijay Ganesan <vijayganesan1@yahoo.ca>
2014-08-10 00:27:57 -0700

    Argh tabbing

--- ws.cpp
+++ ws.cpp
@@ -34,11 +34,11 @@ extern "C" BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserv
 	{
 		case DLL_PROCESS_ATTACH:
 		{
-            char moduleName[MAX_PATH];
+			char moduleName[MAX_PATH];
 			GetModuleBaseName(GetCurrentProcess(), NULL, moduleName, MAX_PATH);
 			if (strcmp(moduleName, "ffxiv.exe"))
-               break;
-            Sleep(10000);
+			   break;
+			Sleep(10000);
 			buf_new = (char*)malloc(MAX_PACKET*sizeof(unsigned char)); //Pre-allocate a buffer
 			CreateThread(NULL,0,setup_console,NULL,0,&threadIDConsole);
 			CreateThread(NULL,0,initialize,NULL,0,&threadID);
@@ -47,7 +47,7 @@ extern "C" BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserv
 		case DLL_PROCESS_DETACH:
 			free(buf_new);
 			VirtualFree((LPVOID)pSend, 0, MEM_RELEASE);
-            VirtualFree((LPVOID)pRecv, 0, MEM_RELEASE);
+			VirtualFree((LPVOID)pRecv, 0, MEM_RELEASE);
 			if (threadIDConsole)
 				PostThreadMessage(threadIDConsole, WM_QUIT, 0, 0);
 #if LOGGING == 1
@@ -64,15 +64,15 @@ extern "C" BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserv
 
 DWORD WINAPI initialize(LPVOID param)
 {
-    DWORD addr;
-    DWORD orig_size;
-    BYTE replaced[10];
+	DWORD addr;
+	DWORD orig_size;
+	BYTE replaced[10];
 	while(!setupFlag);
 	LOG("Initialized!");
 
 	LOG("Replacing WinSock send call");
 	addr = (DWORD)GetProcAddress(GetModuleHandle(TEXT("WS2_32.dll")), "send");
-    LOG("WS2_32.dll:send @ %X",(unsigned int)addr);
+	LOG("WS2_32.dll:send @ %X",(unsigned int)addr);
 	if(apply_patch(0xE9,addr,(void*)(&repl_send),&orig_size, replaced))
 	{
 		pSend = (tWS)VirtualAlloc(NULL, orig_size << 2, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
@@ -81,10 +81,10 @@ DWORD WINAPI initialize(LPVOID param)
 		VirtualProtect((LPVOID)pSend,orig_size+5,PAGE_EXECUTE_READWRITE,NULL); //DEP sucks :(
 		LOG("Success! pSend Address = %X",(unsigned int)pSend);
 	}
-       
-    LOG("Replacing WinSock recv call");
+	   
+	LOG("Replacing WinSock recv call");
 	addr = (DWORD)GetProcAddress(GetModuleHandle(TEXT("WS2_32.dll")), "recv");
-    LOG("WS2_32.dll:recv @ %X",(unsigned int)addr);
+	LOG("WS2_32.dll:recv @ %X",(unsigned int)addr);
 	if(apply_patch(0xE9,addr,(void*)(&repl_recv),&orig_size, replaced))
 	{
 		pRecv = (tWS)VirtualAlloc(NULL, orig_size << 2, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
@@ -94,7 +94,7 @@ DWORD WINAPI initialize(LPVOID param)
 		LOG("Success! pRecv Address = %X",(unsigned int)pRecv);
 	}
 
-#if LOGGING != 2	 	    
+#if LOGGING != 2	 		
 	while(1); //If we aren't logging, then everything has already been setup and we're good to go
 #endif
 	return 0;
@@ -102,40 +102,40 @@ DWORD WINAPI initialize(LPVOID param)
 
 int WINAPI repl_send(SOCKET s, const char *buf, int len, int flags)
 {
-    memcpy(buf_new,buf,len);
-    
+	memcpy(buf_new,buf,len);
+	
 #if LOGGING != 2
 	struct sockaddr_in info;
 	int infolen;
 	getpeername(s,(sockaddr*)(&info),&infolen);
 	const int port = ntohs(info.sin_port);
-    LOG("SEND");
+	LOG("SEND");
 	LOG("%s:%u, Len %d, Flags %d, socket %u",inet_ntoa(info.sin_addr),port,len,flags, s);
 	LOGn("Data: ");  
 	for(int i = 0; i < len; i++) 
 		LOGn("%02X ",(unsigned char)buf_new[i]);
 #endif
 
-    return pSend(s,buf_new,len,flags);
+	return pSend(s,buf_new,len,flags);
 }
 
 int WINAPI repl_recv(SOCKET s, const char *buf, int len, int flags)
 {
-    memcpy(buf_new,buf,len);
-    
+	memcpy(buf_new,buf,len);
+	
 #if LOGGING != 2
 	struct sockaddr_in info;
 	int infolen;
 	getpeername(s,(sockaddr*)(&info),&infolen);
 	const int port = ntohs(info.sin_port);
-    LOG("RECV");
+	LOG("RECV");
 	LOG("%s:%u, Len %d, Flags %d, socket %u",inet_ntoa(info.sin_addr),port,len,flags, s);
 	LOGn("Data: ");  
 	for(int i = 0; i < len; i++) 
 		LOGn("%02X ",(unsigned char)buf_new[i]);
 #endif
 
-    return pRecv(s,buf_new,len,flags);
+	return pRecv(s,buf_new,len,flags);
 }
 
 DWORD WINAPI setup_console(LPVOID param)
@@ -161,5 +161,5 @@ DWORD WINAPI setup_console(LPVOID param)
 		}
 	}
 #endif
-    return 0;
+	return 0;
 }
