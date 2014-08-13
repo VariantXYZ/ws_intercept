caf6a619504c4e24ab89cc9f1bc73b81b8d43246
VariantXYZ <vijayganesan1@yahoo.ca>
2014-08-13 22:40:16 +0000

    Changed new to new_ in list implementation to avoid conflicing with the C++ built-in

--- source/list.h
+++ source/list.h
@@ -36,12 +36,12 @@ struct list_head {
  * This is only for internal list manipulation where we know
  * the prev/next entries already!
  */
-static inline void __list_add(struct list_head *new, struct list_head *prev, struct list_head *next)
+static inline void __list_add(struct list_head *new_, struct list_head *prev, struct list_head *next)
 {
-    next->prev = new;
-    new->next = next;
-    new->prev = prev;
-    prev->next = new;
+    next->prev = new_;
+    new_->next = next;
+    new_->prev = prev;
+    prev->next = new_;
 }
 
 /**
@@ -52,9 +52,9 @@ static inline void __list_add(struct list_head *new, struct list_head *prev, str
  * Insert a new entry after the specified head.
  * This is good for implementing stacks.
  */
-static inline void list_add(struct list_head *new, struct list_head *head)
+static inline void list_add(struct list_head *new_, struct list_head *head)
 {
-    __list_add(new, head, head->next);
+    __list_add(new_, head, head->next);
 }
 
 /**
@@ -65,9 +65,9 @@ static inline void list_add(struct list_head *new, struct list_head *head)
  * Insert a new entry before the specified head.
  * This is useful for implementing queues.
  */
-static inline void list_add_tail(struct list_head *new, struct list_head *head)
+static inline void list_add_tail(struct list_head *new_, struct list_head *head)
 {
-    __list_add(new, head->prev, head);
+    __list_add(new_, head->prev, head);
 }
 
 /*
